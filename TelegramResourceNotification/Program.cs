e14ffe74492b2afd6f59fe3109e9f17cde817e47
Pavel Kazutsin <kazyten@ya.ru>
2022-08-19 21:08:01 +0300

    test

--- TelegramResourceNotification/Program.cs
+++ TelegramResourceNotification/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.WriteLine(DateTime.Now);
+    Thread.Sleep(1000*50);
+}
